723ce72af9821f3feac1c0f6583528ad6ae63be3
Alexeiabianna <alexei.abianna@edu.pucrs.br>
2021-08-31 18:06:55 -0300

    Initial commit

---
+++ springbatchexample/src/test/java/com/alexei/springbatchexample/SpringbatchexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexei.springbatchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
